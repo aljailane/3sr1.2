2ac57df08b0e084d3cccb416c5a360fc0d52805c
Muhammed Aljailane <darhost56@gmail.com>
2020-05-05 07:29:52 +0300

    تحسينات

--- inc/add.php
+++ inc/add.php
@@ -1,6 +1,6 @@
 <small>
-           <a href="add.php" button type="button" class="btn btn-success">اضف صورة</a>
-<a href="<?php echo $urlchat?>" button type="button" class="btn btn-danger">عودة للشـات</a>
-<a href="more.php" button type="button" class="btn btn-primary">المـزيــد</a> 
+  <a href="add.php" button type="button" class="btn btn-success">اضف صورة</a>
+  <a href="<?php echo $urlchat?>" button type="button" class="btn btn-danger">عودة للشـات</a>
+  <a href="more.php" button type="button" class="btn btn-primary">المـزيــد</a> 
 </small>
            
