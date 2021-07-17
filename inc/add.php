9c63bbab4c4e2390964230f52bd49340faeb6fa1
Muhammed Aljailane <darhost56@gmail.com>
2021-07-17 15:21:34 +0300

    Update add.php

--- inc/add.php
+++ inc/add.php
@@ -1,6 +1,6 @@
-<small>
+<p>
   <a href="add.php" button type="button" class="btn btn-success">اضف صورة</a>
   <a href="<?php echo $urlchat?>" button type="button" class="btn btn-danger">عودة للشـات</a>
   <a href="more.php" button type="button" class="btn btn-primary">المـزيــد</a> 
-</small>
+</p>
            
